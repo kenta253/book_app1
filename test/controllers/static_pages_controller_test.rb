3fde3539e8209273a108aac3ce4f68f2a2960a6c
Ubuntu <ubuntu@ip-172-31-27-103.ap-northeast-1.compute.internal>
2021-04-09 08:31:30 +0000

    Add a StaticPages

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get static_pages_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get static_pages_create_url
+    assert_response :success
+  end
+
+end
